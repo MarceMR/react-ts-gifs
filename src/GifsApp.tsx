c60e339c2f132715b30f24b8fa2e29a41bc3fe3a
Fernando Herrera <fernando.herrera85@gmail.com>
2025-06-13 10:54:50 -0400

    feat: implement GIF search functionality with API integration and state management

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -2,19 +2,32 @@ import { useState } from 'react';
 
 import { GifList } from './gifs/components/GifList';
 import { PreviousSearches } from './gifs/components/PreviousSearches';
-import { mockGifs } from './mock-data/gifs.mock';
+
 import { CustomHeader } from './shared/components/CustomHeader';
 import { SearchBar } from './shared/components/SearchBar';
 
+import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.action';
+import type { Gif } from './gifs/interfaces/gif.interface';
+
 export const GifsApp = () => {
-  const [previousTerms, setPreviousTerms] = useState(['dragon ball z']);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
   const handleTermClicked = (term: string) => {
     console.log({ term });
   };
 
-  const handleSearch = (query: string) => {
-    console.log({ query });
+  const handleSearch = async (query: string = '') => {
+    query = query.trim().toLowerCase();
+
+    if (query.length === 0) return;
+
+    if (previousTerms.includes(query)) return;
+
+    setPreviousTerms([query, ...previousTerms].splice(0, 8));
+
+    const gifs = await getGifsByQuery(query);
+    setGifs(gifs);
   };
 
   return (
@@ -35,7 +48,7 @@ export const GifsApp = () => {
       />
 
       {/* Gifs */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </>
   );
 };
